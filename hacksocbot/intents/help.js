f4379873ad0d2eeac2dbbbbd96433859f98ef637
Andrei Belcin <andrei.belcin@student.manchester.ac.uk>
2017-02-18 14:19:56 +0000

    added answers to help.js

--- hacksocbot/intents/help.js
+++ hacksocbot/intents/help.js
@@ -2,9 +2,14 @@ const random = array => { return array[Math.floor(Math.random() * array.length)]
 
 
 const getHelp = () => {
-  const answers = [ 
+  const answers = [
+    'I can help you with the schedule. Or give you directions. And if you need any other sort of assistance, I can always call for help.',
+    'I am here to help you. Just tell me what you need.',
+    'Whenever you need help, just send me a message!',
+    'Want directions, or the timetable? Maybe info on our sponsors! Just ask!',
+    'Need help? Count on me!'
   ]
-  return Promise.resolve([toText(random(answers))])
+  return Promise.resolve([toText(answers[0])])
 }
 const toText = message => { return { type: 'text', content: message } }
 module.exports = getHelp
